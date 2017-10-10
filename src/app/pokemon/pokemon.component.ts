d44fd680a737be136b91ac039e73f7cd67f46c70
Steven Villegas <sagrath23@gmail.com>
2017-10-10 00:34:47 -0500

    added support to indexedDB

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
 import { PokeapiService } from '../services/pokeapi.service';
+import { DatabaseService } from '../services/database.service';
 import { Pokemon } from './pokemon';
 
 import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-pokemon',
-  providers: [PokeapiService],
+  providers: [PokeapiService, DatabaseService],
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.css']
 })
@@ -16,11 +17,20 @@ export class PokemonComponent implements OnInit {
 
   private pokemon: Pokemon;
 
-  constructor(private route: ActivatedRoute, private pokeApi: PokeapiService, private location: Location) { }
+  constructor(private route: ActivatedRoute, private db: DatabaseService, private pokeApi: PokeapiService, private location: Location) { }
 
   ngOnInit(): void {
     this.route.paramMap
-      .switchMap((params: ParamMap) => this.pokeApi.getPokemon(params.get('name')))
+      .switchMap(
+        (params: ParamMap) => {
+          // check if pokemon is in local database
+          return this.db.getPokemonByName(params.get('name'))
+            .then(
+              (pokemon) => pokemon,
+              (error) => {
+                return this.pokeApi.getPokemon(params.get('name'));
+              });
+        })
       .subscribe(pokemon => this.pokemon = pokemon);
   }
 
---
+++ src/app/services/database.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, OnInit } from '@angular/core';
+
+import { AngularIndexedDB } from 'angular2-indexeddb';
+import { Pokemon } from '../pokemon/pokemon';
+
+@Injectable()
+export class DatabaseService {
+
+  private db: AngularIndexedDB;
+
+  constructor() {
+    console.log('creating database...');
+    this.db = new AngularIndexedDB('pokemonDb', 1);
+
+    console.log('open local database');
+    this.db.openDatabase(1, (event) => {
+      console.log('local database opened');
+      const objectStore = event.currentTarget.result.createObjectStore(
+        'pokemons', { keyPath: 'name' , autoIncrement: true });
+
+      objectStore.createIndex('name', 'name', { unique: true });
+    });
+  }
+
+  getPokemonByName(pokemonName: string): Promise<Pokemon> {
+    console.log('searching ' + pokemonName + ' in local database');
+    return this.db.getByKey('pokemons', pokemonName).then(
+      (pokemon) => {
+        return pokemon;
+      }, (error) => {
+        console.log(error);
+      });
+  }
+
+  addPokemon(pokemon: Pokemon): void {
+    this.db.add('pokemons', pokemon).then(
+      () => {
+        console.log('pokemon added to localdatabase');
+      },
+      (error) => {
+        console.log(error);
+      });
+  }
+}
